efa03f8b7a9d9933fc32339d57f6dd9d402e09dd
dzhibas <nikolajus@gmail.com>
2024-01-02 18:25:04 +0100

    more initial commits

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
+use std::time::Duration;
+
 use hidapi::HidApi;
 
-/// splitkb.com vendor and Elora product
+/// splitkb.com vendor id
 const VENDOR_ID: u16 = 0x8d1d;
+/// Elora product id
 const PRODUCT_ID: u16 = 0x9d9d;
 
-fn main() {
+/// How often to refetch new data from dependency services
+const REFRESH_RATE_SECS:u16 = 60;
+
+async fn fetch_stock_tickers() {
+    println!("Run of stock tickers function");
+}
+
+async fn run() {
+    fetch_stock_tickers().await;
+
     println!("Printing hid devices: ");
     match HidApi::new() {
         Ok(api) => {
             for dev in api.device_list() {
                 if dev.vendor_id() == VENDOR_ID && dev.product_id() == PRODUCT_ID {
                     println!(
-                        "{:04x}:{:04x} {:?} {:?}",
+                        "{:03x}:{:04x} {:?} {:?}",
                         dev.vendor_id(),
                         dev.product_id(),
                         dev.manufacturer_string(),
@@ -22,5 +34,14 @@ fn main() {
             }
         }
         Err(_) => eprintln!("Error happened"),
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let mut interval = tokio::time::interval(Duration::from_secs(REFRESH_RATE_SECS.into()));
+    loop {
+        interval.tick().await;
+        run().await;
     }
 }
