3a45ff4cc7b899ff272273d10e734f4aaa10e0da
dzhibas <nikolajus@gmail.com>
2024-01-02 19:55:19 +0100

    Regex querying price from dom

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::time::Duration;
 
 use hidapi::HidApi;
+use regex::Regex;
+use tokio::task;
 
 /// splitkb.com vendor id
 const VENDOR_ID: u16 = 0x8d1d;
@@ -8,20 +10,36 @@ const VENDOR_ID: u16 = 0x8d1d;
 const PRODUCT_ID: u16 = 0x9d9d;
 
 /// How often to refetch new data from dependency services
-const REFRESH_RATE_SECS:u16 = 60;
+const REFRESH_RATE_SECS: u16 = 60;
 
 async fn fetch_stock_tickers() {
     println!("Run of stock tickers function");
+
+    let price =
+        Regex::new("data-symbol=\"TSLA.*?regularMarketPrice.*?value=\"(?<price>.*?)\"").unwrap();
+    let req = reqwest::get("https://finance.yahoo.com/quote/TSLA/").await;
+    let body = req.expect("Request failed").text().await.unwrap();
+
+    let Some(caps) = price.captures(&body) else {
+        print!("No match");
+        return;
+    };
+    println!("{:?}", caps.name("price").unwrap());
 }
 
 async fn run() {
     fetch_stock_tickers().await;
+}
 
-    println!("Printing hid devices: ");
+#[tokio::main]
+async fn main() {
+    let mut found = false;
+
     match HidApi::new() {
         Ok(api) => {
             for dev in api.device_list() {
                 if dev.vendor_id() == VENDOR_ID && dev.product_id() == PRODUCT_ID {
+                    found = true;
                     println!(
                         "{:03x}:{:04x} {:?} {:?}",
                         dev.vendor_id(),
@@ -29,16 +47,18 @@ async fn run() {
                         dev.manufacturer_string(),
                         dev.product_string()
                     );
-                    dbg!(dev);
+                    break;
                 }
             }
         }
         Err(_) => eprintln!("Error happened"),
     }
-}
 
-#[tokio::main]
-async fn main() {
+    if !found {
+        eprintln!("Keyboard not found connected");
+        return;
+    }
+
     let mut interval = tokio::time::interval(Duration::from_secs(REFRESH_RATE_SECS.into()));
     loop {
         interval.tick().await;
