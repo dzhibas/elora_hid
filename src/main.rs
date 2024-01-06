d1f6de566ee09d8b7531cb4d5d83dc1b20b2df52
dzhibas <nikolajus@gmail.com>
2024-01-07 00:46:33 +0100

    Fixing fail scenarios when keyboard disconnectsA:

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::{collections::BTreeMap, error::Error, ffi::CString, time::Duration};
+use std::{collections::BTreeMap, error::Error, time::Duration};
 
-use hidapi::{HidApi, HidError};
+use hidapi::{DeviceInfo, HidApi};
 use regex::Regex;
 
 /// splitkb.com vendor id
@@ -12,7 +12,7 @@ const USAGE_ID: u16 = 0x61;
 const USAGE_PAGE: u16 = 0xFF60;
 
 /// How often to refetch new data from dependency services in seconds
-const REFRESH_RATE_SECS: u16 = 60;
+const REFRESH_RATE_SECS: u16 = 10;
 
 // type alias for stock tickers
 type StockTickerType = BTreeMap<&'static str, f64>;
@@ -64,24 +64,42 @@ fn convert_to_buffer(stocks: StockTickerType) -> Vec<u8> {
     buf
 }
 
+fn find_elora_device(api: &HidApi) -> Option<&DeviceInfo> {
+    let device = api.device_list().find(|&dev| {
+        dev.vendor_id() == VENDOR_ID
+            && dev.product_id() == PRODUCT_ID
+            && dev.usage() == USAGE_ID
+            && dev.usage_page() == USAGE_PAGE
+    });
+    device
+}
+
 /// sends stock ticker to keyboard
-async fn send_to_keyboard(keyboard: &CString, stocks: StockTickerType) -> Result<(), HidError> {
+async fn send_to_keyboard(stocks: StockTickerType) -> Result<(), AppError> {
     println!("Sending to usb keyboard");
+
     let api = HidApi::new()?;
-    let device_info = api
-        .device_list()
-        .find(|&d| d.path().to_owned() == *keyboard);
-    let device = device_info.unwrap().open_device(&api);
+    let device = find_elora_device(&api);
+
+    if device.is_none() {
+        return Err("Device disconnected".into());
+    }
+
+    let device = device.unwrap().open_device(&api);
     let buf = convert_to_buffer(stocks);
     device?.write(&buf)?;
+
     println!("{}", String::from_utf8(buf).unwrap());
     Ok(())
 }
 
 /// Main worker which fetches stuff and sends it to keyboard
-async fn run(keyboard: &CString) -> Result<(), AppError> {
+async fn run() -> Result<(), AppError> {
     let stocks = fetch_stock_tickers().await?;
-    let _ = send_to_keyboard(keyboard, stocks).await;
+    let res = send_to_keyboard(stocks).await;
+    if res.is_err() {
+        eprintln!("Error occured while sending data to keyboard");
+    }
     Ok(())
 }
 
@@ -98,19 +116,9 @@ async fn main() {
     );
 
     let api = HidApi::new().unwrap();
-    let device = api.device_list().find(|&dev| {
-        dev.vendor_id() == VENDOR_ID
-            && dev.product_id() == PRODUCT_ID
-            && dev.usage() == USAGE_ID
-            && dev.usage_page() == USAGE_PAGE
-    });
+    let device = find_elora_device(&api);
 
-    let mut interface: Option<CString> = None;
-    if let Some(dev) = device {
-        interface = Some(dev.path().to_owned());
-    }
-
-    if interface.is_none() {
+    if device.is_none() {
         eprintln!("Error: Elora keyboard not found connected");
         return;
     }
@@ -118,7 +126,7 @@ async fn main() {
     let mut interval = tokio::time::interval(Duration::from_secs(REFRESH_RATE_SECS.into()));
     loop {
         interval.tick().await;
-        let _ = run(&interface.clone().unwrap()).await;
+        let _ = run().await;
     }
 }
 
